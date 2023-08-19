d660a1f43791e1d1aaa669c6ebc57006aa860747
Jose <diazmarinjoseelias2007@gmail.com>
2023-08-19 17:50:33 -0500

    Upgrade

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -21,16 +21,16 @@ export const Navbar = () => {
     }
 
     return (
-        <nav className="w-full bg-gray-950 dark:bg-slate-300 p-5 md:p-10 transition duration-300">
+        <nav className="w-full bg-gray-950 dark:bg-slate-300 py-3 px-6 md:py-6 md:px-6 transition duration-300">
             <div className="flex justify-center">
-                <h1 className="text-2xl md:text-3xl text-center dark:text-gray-900">Morse translator</h1>
+                <h1 className="text-2xl md:text-3xl text-center dark:text-gray-900 my-2.5">Morse translator</h1>
                 
                 <div className="ms-auto flex gap-2">
-                    <a href="https://github.com/jeliasdiaz/morse-app" target="_blank" rel="noreferrer"><BsGithub size={35} className="ms-auto hover:bg-gray-700/50 p-1.5 rounded-md cursor-pointer dark:text-gray-900 dark:hover:bg-gray-400/50" /></a>
+                    <a href="https://github.com/jeliasdiaz/morse-app" target="_blank" rel="noreferrer"><BsGithub size={60} className="ms-auto hover:bg-gray-700/50 p-4 rounded-md cursor-pointer dark:text-gray-900 dark:hover:bg-gray-400/50" /></a>
                 {
                     themeIcon
-                        ? <HiMoon className="hover:bg-gray-700/50 p-1.5 rounded-md cursor-pointer dark:text-gray-900 dark:hover:bg-gray-400/50" size={35} onClick={toggleIconTheme} />
-                        : <HiSun className="hover:bg-gray-700/50 p-1.5 rounded-md cursor-pointer dark:text-gray-900 dark:hover:bg-gray-400/50" size={35} onClick={toggleIconTheme}/>
+                        ? <HiMoon className="hover:bg-gray-700/50 p-4 rounded-md cursor-pointer dark:text-gray-900 dark:hover:bg-gray-400/50 transition duration-200" size={60} onClick={toggleIconTheme} />
+                        : <HiSun className="hover:bg-gray-700/50 hover:rotate-45 p-4 rounded-md cursor-pointer dark:text-gray-900 dark:hover:bg-gray-400/50 transition duration-200" size={60} onClick={toggleIconTheme}/>
                 }
                 </div>
             </div>
